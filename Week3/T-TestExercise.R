786e475c7764d02fc3192bd8056558356a6279cc
Ujjwal Peshin <ujjwal.peshin@zs.com>
2017-10-30 20:22:30 +0530

    Third Week Started

---
+++ Week3/T-TestExercise.R
@@ -0,0 +1,19 @@
+install.packages('downloader')
+library(downloader) 
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/babies.txt"
+filename <- basename(url)
+download(url, destfile=filename)
+babies <- read.table("babies.txt", header=TRUE)
+library(dplyr)
+bwt.nonsmoke <- filter(babies, smoke==0) %>% select(bwt) %>% unlist 
+bwt.smoke <- filter(babies, smoke==1) %>% select(bwt) %>% unlist
+library(rafalib)
+mean(bwt.nonsmoke)-mean(bwt.smoke)
+popsd(bwt.nonsmoke)
+popsd(bwt.smoke)
+#q1
+set.seed(1)
+dat.ns <- sample(bwt.nonsmoke, size = 25)
+dat.s <- sample(bwt.smoke, size = 25)
+tval <- t.test(dat.ns,dat.s)
+tval
