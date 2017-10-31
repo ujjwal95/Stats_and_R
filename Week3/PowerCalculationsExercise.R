8490ffae867ba45c56e835bbd7e2a582e57b5ac0
Ujjwal Peshin <ujjwal.peshin@zs.com>
2017-10-31 19:31:47 +0530

    Third Week Halfway Done

--- Week3/PowerCalculationsExercise.R
+++ Week3/PowerCalculationsExercise.R
@@ -12,3 +12,22 @@ mean(bwt.nonsmoke)-mean(bwt.smoke)
 popsd(bwt.nonsmoke)
 popsd(bwt.smoke)
 
+#q1
+set.seed(1)
+dat.ns <- sample(bwt.nonsmoke,size = 5)
+dat.s <- sample(bwt.smoke,size = 5)
+print(t.test(dat.ns,dat.s)$p.value)
+
+#q2
+set.seed(1)
+B <- 10000
+N <- 5
+alpha <- 0.05
+reject <- function(N, alpha = 0.05){
+  dat.ns <- sample(bwt.nonsmoke,size = N)
+  dat.s <- sample(bwt.smoke,size = N)
+  p_value <- t.test(dat.ns,dat.s)$p.value
+  p_value < alpha
+}
+rejections <- replicate(B, reject(N, alpha))
+mean(rejections)
